e39334159b3b509da93e45a3925e2a55b6b91a01
Nebobzod <nebobzod@gmail.com>
2023-09-14 11:26:48 +0300

    STable exercise settings improved + <b>Fist APK</b>
    	SchulteSettingsFragment
    	-- done getting xSize, ySize
    	(todo: update with no focus-lost also -- done with onPause calling view..getOnFocusChangeListener().onFocusChange(view, true);)
    
    	Version is set
    	<b>Fist APK is generated and tested within two devices</b>
    	strings.xml is neaten
    	version 0.02 is set
    	todo: make plan (main menu: user, about, more neat runner-settings, runner-settings for basics, tempo-bar, news, mistakes detail level, stat, chance settings)
    
    	current task: trying to use settings Fragment to basics-exercise-options
        	org.nebobrod.schulteplus.ui.basics.BasicSettings
        	and R.xml.root_preferences

--- app/src/main/java/org/nebobrod/schulteplus/ui/schultesettings/SchulteSettingsFragment.java
+++ app/src/main/java/org/nebobrod/schulteplus/ui/schultesettings/SchulteSettingsFragment.java
@@ -1,7 +1,11 @@
 package org.nebobrod.schulteplus.ui.schultesettings;
 
+import static org.nebobrod.schulteplus.Utils.intFromString;
+
+
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,21 +15,94 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.nebobrod.schulteplus.ExerciseRunner;
 import org.nebobrod.schulteplus.R;
 
 public class SchulteSettingsFragment extends Fragment {
 
 	private SchulteSettingsViewModel mViewModel;
+	private View view;
+	private EditText etWidth, etHeight;
+	private ExerciseRunner runner = ExerciseRunner.getInstance(getContext());
+	String[] exTypes;
+
+/*	Resources res = getResources();
+	TypedArray exTypes = res.obtainTypedArray(R.array.ex_type);
+	@StyleableRes  int index = 0;
+	String exType = exTypes.getString(index) ;*/
 
 	public static SchulteSettingsFragment newInstance() {
 		return new SchulteSettingsFragment();
 	}
 
 	@Override
+	public void onPause() {
+//		Toast.makeText(getContext(), "onPause", Toast.LENGTH_SHORT).show();
+		// here we call overridden methods to complete settings
+		etWidth.getOnFocusChangeListener().onFocusChange(etWidth, true);
+		etHeight.getOnFocusChangeListener().onFocusChange(etHeight, true);
+		runner.setExType(exTypes[0]);
+		super.onPause();
+	}
+
+
+	/*@Override
+	public void onStop() {
+		Toast.makeText(getContext(), "onStop", Toast.LENGTH_SHORT).show();
+		super.onStop();
+	}*/
+
+
+	/*@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_SHORT).show();
+		super.onSaveInstanceState(outState);
+	}*/
+
+	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
 							 @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_schulte_settings, container, false);
+		view = inflater.inflate(R.layout.fragment_schulte_settings, container, false);
+
+		Resources res = getResources();
+		exTypes = res.getStringArray(R.array.ex_type);
+
+		etWidth = view.findViewById(R.id.et_x_size);
+		etWidth.setText("" + runner.getX());
+		etHeight= view.findViewById(R.id.et_y_size);
+		etHeight.setText("" + runner.getY());
+
+		// set X & Y values to Runner (and for future: https://stackoverflow.com/a/8063840/20456581)
+		etWidth.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View view, boolean b) {
+				byte value = (byte) intFromString("" + etWidth.getText());
+				if (value < 2 || value > 10){
+					Toast.makeText(getContext(), R.string.hnt_input_height, Toast.LENGTH_SHORT).show();
+					value = 5;
+					etWidth.setText("" + value);
+				}
+				runner.setX(value);
+			}
+		});
+		etHeight.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View view, boolean b) {
+				byte value = (byte) intFromString("" + etHeight.getText());
+				if (value < 2 || value > 10){
+					Toast.makeText(getContext(), R.string.hnt_input_height, Toast.LENGTH_SHORT).show();
+					value = 5;
+					etHeight.setText("" + value);
+				}
+				runner.setY(value);
+			}
+		});
+		// they say it has to be cleaned after usage
+//		exTypes.recycle();
+		return view;
 	}
 
 	@Override
@@ -33,6 +110,7 @@ public class SchulteSettingsFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 		mViewModel = new ViewModelProvider(this).get(SchulteSettingsViewModel.class);
 		// TODO: Use the ViewModel
+
 	}
 
 }

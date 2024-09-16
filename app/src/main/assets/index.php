4d71b30e4d7759a3ff944208041214cbf6ac289e
Nebobzod <nebobzod@gmail.com>
2024-09-16 23:25:48 +0300

    sssr-space
    html corrections
    
    android:versionCode="114"

--- app/src/main/assets/index.php
+++ app/src/main/assets/index.php
@@ -7,7 +7,7 @@
   -->
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" prefix="og: http://ogp.me/ns#">
 <head>
     <meta charset="UTF-8">
     <title>Smart Rovers</title>
@@ -31,6 +31,9 @@
     <meta name="twitter:description" content="The independent development to Improve your consciousness">
     <meta name="twitter:image" content="http://attplus.in/common/ic_launcher_round.png">
 
+    <link rel="icon" href="http://attplus.in/favicon.xml" type="image/svg+xml">
+    <link rel="icon" href="http://attplus.in/favicon.png" type="image/png">
+
     <!-- Разметка JSON-LD, созданная Мастером разметки структурированных данных Google. -->
     <script type="application/ld+json">
     [
@@ -43,6 +46,24 @@
       }
     ]
     </script>
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();
+       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(98319797, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+       });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/98319797" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
 </head>
 <body>
 <?php
